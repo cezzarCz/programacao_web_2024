25508ea7dd0d9343dc07c28a8b8b63df2b5ffc40
ERICK CEZAR SEABRA DA SILVA <erick.seabra@a.ucb.br>
2024-03-14 10:49:09 -0300

    Subindo atividade 1

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+let var1 = 5
+let var2 = 6
+var resultado = var1 + var2
+
+console.log("Soma entre " + var1 + " e " + var2 + " = " + resultado)
+
+var resultado = var1 - var2
+
+console.log("Subtração entre " + var1 + " e " + var2 + " = " + resultado)
+
+var resultado = var1 * var2
+
+console.log(`Multiplicação entre ${var1} e ${var2} = ${resultado}`)
+
+var resultado = var1 / var2
+
+console.log(`Divisão entre ${var1} e ${var2} = ${resultado}`)
